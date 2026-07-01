cf3b933042ff96a2c38da906183879fc1609c6bf
Chris <chris@example.com>
2026-07-01 22:50:08 +0900

    perf: use mp3 format instead of wav for faster soundfont loading

--- guitar-synth.js
+++ guitar-synth.js
@@ -19,13 +19,13 @@ async function getGuitarSynth() {
 
             _guitarSampler = new Tone.Sampler({
                 urls: {
-                    "E2": "E2.wav",
-                    "A2": "A2.wav",
-                    "C3": "C3.wav",
-                    "E3": "E3.wav",
-                    "A3": "A3.wav",
-                    "C4": "C4.wav",
-                    "F4": "F4.wav"
+                    "E2": "E2.mp3",
+                    "A2": "A2.mp3",
+                    "C3": "C3.mp3",
+                    "E3": "E3.mp3",
+                    "A3": "A3.mp3",
+                    "C4": "C4.mp3",
+                    "F4": "F4.mp3"
                 },
                 baseUrl: "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/guitar-acoustic/",
                 onload: () => {
